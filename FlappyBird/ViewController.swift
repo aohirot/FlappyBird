e9a7187f6af49a8ed08def7ea05b24c191d7181f
hiromichi.aoki <hiromichiaoki19930426@aol.com>
2020-10-27 08:56:50 +0900

    Initial Commit

---
+++ FlappyBird/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlappyBird
+//
+//  Created by Hiromichi Aoki on 10/27/20.
+//  Copyright © 2020 Hiromichi Aoki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
